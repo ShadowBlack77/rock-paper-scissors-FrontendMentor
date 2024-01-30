1931a44cdab5ffe3581d328f9dbb5fd7adbb8deb
ShadowBlack <daniel.miliczek@op.pl>
2024-01-30 12:55:45 +0100

    Html & CSS

---
+++ rock-paper-scissors-master/app.js
@@ -0,0 +1,6 @@
+const rules = document.querySelector('.rules');
+const wrapper = document.querySelector('.wrapper');
+const wrapperRules = document.querySelector('.wrapperRules');
+const closeBtn = document.querySelector('.close');
+const pickAi = document.querySelector('.pick--ai');
+const pickContainerAi = document.querySelector('.pick__container--ai');
